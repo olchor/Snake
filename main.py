311785c15e2cca15fc5baf8c51a5b060bf1ee5fb
Olaf <olaf0699@interia.pl>
2023-01-13 19:08:01 +0100

    Generating food on the screen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame, os
+import numpy as np
 pygame.font.init()
 
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = 800, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Snake')
 
@@ -30,10 +31,10 @@ class Food:
         self.img = FOOD
         self.x = x
         self.y = y
-        self.mask = pygame.mask.from_surface(self.img)
+        #self.mask = pygame.mask.from_surface(self.img)
 
     def draw(self, window):
-        window.blit(self.img, (self.x[0], self.y[0])) 
+        window.blit(self.img, (self.x, self.y)) 
 
 class Player:
 
@@ -87,18 +88,45 @@ class Player:
             if self.x[0] == self.x[i] and self.y[0] == self.y[i]:
                 quit()
 
-def main(speed):
+def main(spd):
     run = True
     score = 0
     main_font = pygame.font.SysFont('arial', 50)
     clock = pygame.time.Clock()
-    
-    movement_speed = 30 / (speed+1)
+    food_x, food_y = 0, 0
 
-    player = Player(500, 500, 20)
+    movement_speed = 30 / (spd+1)
 
-    def food_location():
-        pass
+    stage_matrix = np.zeros((45,40))
+
+    #This part of the code exclude section at the top where score is located
+    for i in range(0, 40):
+        for j in range(0, 6):
+            stage_matrix[j, i] = 3
+
+    player = Player(600, 500, 20)
+    food = Food(int(food_x), int(food_y))
+
+
+    def player_location():
+        for i in range(0, len(player.x)):
+            stage_matrix[int(player.x[i]/20), int(player.y[i]/20)] = 1
+        
+
+    def food_location(matrix):
+        matrix = matrix.flatten()
+
+        empty_space = np.array(np.where(matrix == 0))
+        empty_space = empty_space.flatten()
+
+        food_space = np.random.choice(empty_space)
+
+        matrix[food_space] = 2
+        matrix = np.reshape(matrix, (40, 45))
+
+        global food_y, food_x
+        food_y, food_x = np.array(np.where(matrix == 2))
+
 
     def redraw_window():
         WIN.blit(BACKGROUND_GAME, (0,0))
@@ -107,10 +135,15 @@ def main(speed):
        
         WIN.blit(score_label, (10,10))
 
+        player_location()
+        food_location(stage_matrix)
+
         player.cooldown(movement_speed)
         player.movement()
         player.collide()
         player.draw(WIN)
+
+        food.draw(WIN)
 
         pygame.display.update()
 
@@ -151,7 +184,7 @@ def main(speed):
 def main_menu():
     title_font = pygame.font.SysFont('arial', 75)
     menu_font = pygame.font.SysFont('arial', 40)
-    menu_y = [200, 400, 600]
+    menu_y = [250, 500, 750]
     cursor_location = 0
     cursor_x = 300
     cursor_y = menu_y[cursor_location]
@@ -167,7 +200,7 @@ def main_menu():
         speed_value_label = menu_font.render(f'{speed_options[speed]}', 1, (255, 255, 255))
         quit_label = menu_font.render('Quit', 1, (255,255,255))
 
-        WIN.blit(title_label, ((WIDTH - title_label.get_width())/2, 10))
+        WIN.blit(title_label, ((WIDTH - title_label.get_width())/2, 20))
         WIN.blit(start_label, ((WIDTH - start_label.get_width())/2, menu_y[0]))
         WIN.blit(speed_label, ((WIDTH - speed_label.get_width())/2, menu_y[1]))
         WIN.blit(speed_value_label, ((WIDTH - speed_label.get_width())/2 + WIDTH/4, menu_y[1]))
